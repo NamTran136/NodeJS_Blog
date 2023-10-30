c793d67bd8e4e20ed9e2b534dd6d87694d107f3b
Nam Tran <namnam13130606@gmail.com>
2023-10-30 21:14:23 +0700

    Refactor code: create routes, app/controllers folder

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+const route = require('./routes')
+
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
+// XMLHttpRequest, fetch, axios
 
 //HTTP logger
 app.use(morgan('combined'))
@@ -16,13 +25,9 @@ app.engine('hbs', handlebars.engine(
 ))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
-app.get('/', (req, res) => {
-  res.render('home')
-})
 
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+// Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
